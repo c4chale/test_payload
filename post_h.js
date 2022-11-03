b520a1c138e18b9a266b33c083d30fb2f248088e
Alessandro Candeloro <32636582+c4chale@users.noreply.github.com>
2022-11-03 13:03:41 +0100

    Update and rename post2.js to post_h.js

--- post_h.js
+++ post_h.js
@@ -1,10 +1,13 @@
+const myHeaders = new Headers();
+myHeaders.append('Access-Control-Allow-Origin', '*');
+myHeaders.set('Access-Control-Allow-Origin', '*');
+
 (async () => {
     const rawResponse = await fetch('https://crossorigin.me/https://www.cassaforenze.it/?rid=mDPEqCa', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({a: 1, b: 'Textual content'})
     });
